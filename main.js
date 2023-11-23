d474ef8fea471cb3be8cc28c2f6fcf9845a3057d
olopcodes <olopcodes@gmail.com>
2023-11-22 23:00:03 -0500

    toggle spinner

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const removeSpinner = () => {
 };
 
 const fetchAdvice = async () => {
-  //   spinnerEl.classList.toggle("spinner-hide");
+  spinnerEl.classList.toggle("spinner-hide");
 
   try {
     const res = await fetch("https://api.adviceslip.com/advice");
